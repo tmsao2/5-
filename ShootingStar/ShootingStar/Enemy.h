8e4c5aee2b3a3c5a10ad3217bdfbfc634760e00b
高須真樹 <masaki.takasu0727@gmail.com>
2020-05-19 02:31:32 +0900

    敵追加

--- ShootingStar/ShootingStar/Enemy.h
+++ ShootingStar/ShootingStar/Enemy.h
@@ -3,8 +3,16 @@
 class Enemy :
 	public Object
 {
+protected:
+	//頂点バッファビュー
+	D3D12_VERTEX_BUFFER_VIEW _vbView = {};
+	bool VertexBufferInit(Microsoft::WRL::ComPtr<ID3D12Device> dev);
+	bool InitPipeLine(Microsoft::WRL::ComPtr<ID3D12Device> dev);
 public:
 	Enemy(Microsoft::WRL::ComPtr<ID3D12Device> dev);
 	~Enemy();
+	void Update();
+	void Draw(ID3D12Device* dev, ID3D12GraphicsCommandList* cmd, D3D12_VIEWPORT& view, D3D12_RECT& rect,
+		ID3D12DescriptorHeap* wvp);
 };
 
